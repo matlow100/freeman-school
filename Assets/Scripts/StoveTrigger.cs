43365f3bcd4ee1bdea2f28fb61a10abd611bce24
Matt Lowe <matlow100@yahoo.com>
2021-04-23 16:15:15 -0500

    Added nametag and stoveTrigger script

---
+++ Assets/Scripts/StoveTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoveTrigger : MonoBehaviour
+{
+    public GameObject stoveTrigger;
+    bool activated = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            stoveTrigger.SetActive(true);
+            activated = true;
+            Debug.Log("The wood has left the pile");
+        }
+    }
+}
