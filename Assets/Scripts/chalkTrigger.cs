7eb81fc2795f78b4ecab63cd5f405a6b8d9afea5
Matt Lowe <matlow100@yahoo.com>
2021-04-28 16:08:43 -0500

    Added additional chalk task

---
+++ Assets/Scripts/chalkTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chalkTrigger : MonoBehaviour
+{
+    public GameObject writing;
+    bool activated = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("ChalkObj"))
+        {
+            if (!activated)
+            {
+                writing.SetActive(true);
+                activated = true;
+            }
+        }
+    }
+}
