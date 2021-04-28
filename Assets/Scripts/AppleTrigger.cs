8f4c5a87cc804979e45d373889772da934cf0613
Matt Lowe <matlow100@yahoo.com>
2021-04-28 16:40:15 -0500

    Began adding apple task

---
+++ Assets/Scripts/AppleTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTrigger : MonoBehaviour
+{
+    // TODO
+    bool activated = false;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("AppleObj"))
+        {
+            if (!activated)
+            {
+                // TODO
+                Debug.Log("Apple placed");
+                activated = true;
+            }
+        }
+    }
+}
